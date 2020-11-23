0e2a70b9b309b6e8247aae3e2c6711be93849af7
GerardoGepda <gerardo.gepda@gmail.com>
2020-11-22 19:38:56 -0600

    comit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import tasks from './samples/tasks.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: tasks,
+    };
+  }
+
+  render () {
+    return(
+      <div>
+        {this.state.tasks.map(e => 
+          <p>{e.title} - {e.description} - {e.done} - {e.id}</p>  
+        )}
+      </div>
+    );
+  };
 }
 
 export default App;
